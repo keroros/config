37b89387bb911105565b64dc7a7826f8644eb9e5
keroros <2862478683@qq.com>
2024-10-06 01:03:53 +0800

    update script

---
+++ script/tmux_start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+tmux new-session -d -s mysession
+
+tmux split-window -v
+
+tmux select-pane -t 0
+tmux split-window -h
+
+tmux select-pane -t 2
+tmux split-window -h
+
+tmux select-pane -t 0
+
+tmux attach-session -t mysession
